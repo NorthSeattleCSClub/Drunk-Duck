3d25c7039519e1b96c1626c1033efb3b9524867e
ONevets <otraigusnevets@gmail.com>
2018-03-01 15:25:08 -0800

    3/1/2018 changes
    platform = 90% done
    playermovement = done
    cameracontrol = done
    sprites = 80% done

---
+++ Assets/Scripts/HeyItsPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeyItsPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject platformObject;
+
+	public int numberPlatform;
+	public float levelWidth = 3f;
+	public float minY = .2f;
+	public float maxY = 1.5f;
+
+	void Start () {
+
+		Vector3 positionSpawn = new Vector3();
+
+		for (int i = 0	; i < numberPlatform; i++) {
+			positionSpawn.y += Random.Range(minY, maxY);
+			positionSpawn.x = Random.Range (-levelWidth, levelWidth);
+			Instantiate (platformObject, positionSpawn, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
